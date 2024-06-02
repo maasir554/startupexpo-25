fb0aee176ed6a675283ca601d75d81e302545760
MohammadMaasir <maasir554@gmail.com>
2024-06-03 02:39:37 +0530

    Corrections...

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //carousel
 const hamburgerButton = document.querySelector("#hamburger-menu");
+const navBar = document.querySelector('nav')
 const MobileMenu = document.querySelector("#mobile-menu");
 let isMenuOpened = false;
 
+const closeMenu = () => {
+    hamburgerButton.children[0].classList.remove("cross-bar-1")
+    hamburgerButton.children[1].style.opacity = '1'
+    hamburgerButton.children[2].classList.remove("cross-bar-2")
+    MobileMenu.style.transform = "translateX(-100%)"
+    isMenuOpened = false;
+    document.body.style.overflow = ""
+}
+
 const HamburgerButtonHandeler = () => {
     if(isMenuOpened){
-        hamburgerButton.children[0].classList.remove("cross-bar-1")
-        hamburgerButton.children[1].style.opacity = '1'
-        hamburgerButton.children[2].classList.remove("cross-bar-2")
-        MobileMenu.style.transform = "translateX(-100%)"
-        isMenuOpened = false;
+        closeMenu();
     }
     else{
         hamburgerButton.children[0].classList.add("cross-bar-1")
@@ -17,12 +23,14 @@ const HamburgerButtonHandeler = () => {
         hamburgerButton.children[2].classList.add("cross-bar-2")
         MobileMenu.style.transform = "translateX(0)"
         isMenuOpened = true;
+        document.body.style.overflow = "hidden";
     }
 }
 
 hamburgerButton.addEventListener('click', HamburgerButtonHandeler);
+console.log(MobileMenu.children[0].children);
 
-const navBar = document.querySelector('nav')
+Array.from(MobileMenu.children[0].children).forEach(li => li.addEventListener("click", closeMenu))
 
 let lastPosn = 0;
 window.addEventListener("scroll", () => {

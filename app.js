74dd23c7d183c301338e8cbad5ba321a4cfb5599
MohammadMaasir <maasir554@gmail.com>
2024-06-03 16:43:53 +0530

    Some minor refinements

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-//carousel
+//Programming fpr navBar disappear on scroll down
+
+let lastPosn = 0;
+
+const NavEffect = (evt) => {
+    if (window.scrollY > lastPosn){
+        navBar.style.transform = "translateY(-100%)";
+    }
+    else {
+        navBar.style.transform = "translateY(0)";
+    }
+    lastPosn = window.scrollY;
+}
+
+window.addEventListener("scroll",NavEffect)
+
+
+// For Hamburber button and menu: 
+
 const hamburgerButton = document.querySelector("#hamburger-menu");
 const navBar = document.querySelector('nav')
 const MobileMenu = document.querySelector("#mobile-menu");
 let isMenuOpened = false;
 
 const closeMenu = () => {
+    window.addEventListener("scroll",NavEffect)
     hamburgerButton.children[0].classList.remove("cross-bar-1")
     hamburgerButton.children[1].style.opacity = '1'
     hamburgerButton.children[2].classList.remove("cross-bar-2")
@@ -18,6 +37,7 @@ const HamburgerButtonHandeler = () => {
         closeMenu();
     }
     else{
+        window.removeEventListener("scroll",NavEffect)
         hamburgerButton.children[0].classList.add("cross-bar-1")
         hamburgerButton.children[1].style.opacity = '0'
         hamburgerButton.children[2].classList.add("cross-bar-2")
@@ -31,19 +51,7 @@ hamburgerButton.addEventListener('click', HamburgerButtonHandeler);
 
 Array.from(MobileMenu.children[0].children).forEach(li => li.addEventListener("click", closeMenu))
 
-let lastPosn = 0;
-window.addEventListener("scroll", () => {
-
-    if (window.scrollY > lastPosn){
-        navBar.style.transform = "translateY(-100%)";
-    }
-    else {
-        navBar.style.transform = "translateY(0)";
-    }
-    lastPosn = window.scrollY;
-})
-
-// progrsmming for count-down timer
+// programming for count-down timer
 
 const   MainCountdownBox = document.getElementById("countdown")
         DaysBox = document.getElementById("count-days"),

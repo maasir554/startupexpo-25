98ff36ea8193d6d9f4fb3f0ced39b65c823e2c78
Sanskar Shrivastava <77103089+Sanschinu95@users.noreply.github.com>
2024-12-11 14:20:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const   MainCountdownBox = document.getElementById("countdown")
         MinutesBox = document.getElementById("count-mins"),
         SecsBox = document.getElementById("count-secs");
 
-const EventDate = new Date("2024-06-18T09:00:00")
+const EventDate = new Date("2025-02-14T09:00:00")
 
 let currentDate = Date.now()
 

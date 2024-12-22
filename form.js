44263a14fd24e4b650538229de0cfffed2c8e86f
MohammadMaasir <maasir554@gmail.com>
2024-12-23 00:32:58 +0530

    basic config expo form

---
+++ form.js
@@ -0,0 +1,29 @@
+
+const myForm = document.querySelector("#registration-form-element");
+
+myForm.addEventListener("submit", async (evt) => {
+    evt.preventDefault();
+    const form = evt.target
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+    
+    try {
+        const response = await fetch('https://server.ecellnitb.in/startup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(data), 
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log('Form submitted successfully:', result);
+        } else {
+            console.error('Error submitting form:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+
+})
